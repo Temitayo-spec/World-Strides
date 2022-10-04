aeef1feae6d6142531929b47545548809071a75f
unknown <olawanletemitayo@gmail.com>
2022-10-04 23:06:54 +0100

    made the ui more nicer

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -54,7 +54,6 @@ const Sidebar = ({
         </div>
       ) : (
           <div style={{
-            display: "flex",
             padding: "10px",
         }}>
           <FormControl className={classes.formControl}>
